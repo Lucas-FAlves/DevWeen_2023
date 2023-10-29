053bd633337053355e2fd05f116d1a17908a8ad5
MatheusCapucho <matheusfcapucho@gmail.com>
2023-10-29 17:11:13 -0300

    Player Animation

--- Assets/_Scripts/Animation/PlayerAnimation.cs
+++ Assets/_Scripts/Animation/PlayerAnimation.cs
@@ -8,6 +8,7 @@ public class PlayerAnimation : MonoBehaviour
 {
     private PlayerInputActions inputActions;
     private Animator animator;
+    private Vector2 lastDir;
     void Awake()
     {
         inputActions = new PlayerInputActions();
@@ -18,15 +19,32 @@ public class PlayerAnimation : MonoBehaviour
     private void OnEnable()
     {
         inputActions.Player.Move.performed += ChangeAnimation;
+        inputActions.Player.Move.canceled += CancelAnimation;
     }
     private void OnDisable()
     {
         inputActions.Player.Move.performed -= ChangeAnimation;
+        inputActions.Player.Move.canceled -= CancelAnimation;
+    }
+
+    private void CancelAnimation(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<Vector2>();
+        animator.SetFloat("xInputStop", direction.x);
+        animator.SetFloat("yInputStop", direction.y);
     }
 
     private void ChangeAnimation(InputAction.CallbackContext context)
     {
         var direction = context.ReadValue<Vector2>();
+        lastDir = direction;
+        animator.enabled = true;
+        if (direction == Vector2.zero)
+        {
+            animator.enabled = false;
+            return;
+        }
+
         animator.SetFloat("xInput", direction.x);
         animator.SetFloat("yInput", direction.y);
     }
